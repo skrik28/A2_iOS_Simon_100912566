cc00a6ff696627d0464682872cf4da03e0d55135
Simon Kriksciunas <100912566@georgebrown.ca>
2025-03-26 14:32:36 -0700

    updated ProductDetailViewController with outlets, properties, core data context

--- A2_iOS_Simon_100912566/ViewControllers/ProductDetailViewController.swift
+++ A2_iOS_Simon_100912566/ViewControllers/ProductDetailViewController.swift
@@ -4,4 +4,24 @@
 //
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class ProductDetailViewController: UIViewController {
+    // MARK: outlets
+    @IBOutlet weak var productIDLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // MARK: properties
+    var products: [Product] = []
+    var currentIndex = 0
+    
+    // MARK: Core Data Context
+    var context: NSManagedObjectContext {
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+}
